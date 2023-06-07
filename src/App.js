61165097ddbe20209271c56313bf566e7fedb007
saied2035 <saied1998@gmail.com>
2023-06-07 18:37:27 +0300

    add linters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-function App() {
-  return (
-    <p className="text-3xl font-bold underline">
-      Hello world!
-    </p>
-  );
-}
+const App = () => (
+  <p className="text-3xl font-bold underline">
+    Hello world!
+  </p>
+);
 
 export default App;
