9f20637455008ce6411418c805e2aa40342af8d2
saied2035 <saied1998@gmail.com>
2023-08-15 22:55:27 +0300

    prepare home page

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,11 +1,17 @@
 import Navbar from '../navbar/navbar';
+import AirlinesContainer from './airlinesContainer/airlinesContainer';
 
 const HomePage = () => (
   <main className="flex">
     <Navbar />
-    <p className="text-3xl font-bold underline">
-      Home Page!
-    </p>
+    <section className="flex flex-col flex-auto">
+      <h1 className="[word-spacing:4px] flex flex-col gap-y-2 justify-end text-center text-4xl tracking-wide font-bold basis-48 self-center font-['Repo']">
+        Top Airlines
+        <span className="text-xs tracking-wider text-[#b9b9b9]">Please select an Airline</span>
+      </h1>
+      <hr className="border-0 border-b-2 border-dotted w-28 mx-auto mt-8" />
+      <AirlinesContainer />
+    </section>
   </main>
 );
 
