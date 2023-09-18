d1c9cfa1ce51a80187db843835b167a5c8f262a0
saied2035 <saied1998@gmail.com>
2023-09-18 16:12:48 +0300

    Prepare reverse an airline page

---
+++ src/container/components/reserveAirline/reserveAirline.js
@@ -0,0 +1,9 @@
+const ReserveAirline = () => (
+  <section className="flex-auto relative bg-[url('./container/components/airplane.png')] bg-[length:80%_auto]
+    bg-center bg-no-repeat z-10"
+  >
+    <div className="absolute w-full h-full bg-[#96bf01] opacity-90 z-[-1]" />
+  </section>
+);
+
+export default ReserveAirline;
