9d964ca1665deec2eeab757532cba0c4f3f0a0c7
saied2035 <saied2421998@gmail.com>
2023-10-25 12:29:29 +0300

    Add action to send api request to create airline

--- src/redux/airlineSlice/airlineSlice.js
+++ src/redux/airlineSlice/airlineSlice.js
@@ -1,12 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import {
   isLoggedIn, login, signout, signup,
 } from '../authSlice/authSlice';
+import airlinesRequests from './airlineApi/airlineApi';
+
+export const createAirline = createAsyncThunk(
+  'airlines/createAirline', async (body) => {
+    const response = await airlinesRequests.createAirline(body);
+    console.log({ response });
+    return response;
+  },
+);
 
 const airlineSlice = createSlice({
   name: 'airlines',
   initialState: {
-    airlines: [], user_airlines: [], airlines_length: 0, user_airlines_length: 0,
+    airlines: [],
+    user_airlines: [],
+    airlines_length: 0,
+    user_airlines_length: 0,
+    pending: false,
+    error: '',
   },
   extraReducers: (builder) => {
     // signup
@@ -43,6 +57,42 @@ const airlineSlice = createSlice({
         modifier.airlines_length = action.payload.airlines.length;
         modifier.user_airlines = action.payload.user_airlines;
         modifier.user_airlines_length = action.payload.user_airlines.length;
+      }
+    });
+    // create airline
+    builder.addCase(createAirline.pending, (state) => {
+      const modifier = state;
+      modifier.pending = true;
+    });
+
+    builder.addCase(createAirline.fulfilled, (state, action) => {
+      const modifier = state;
+      if (action.payload.airlines) {
+        modifier.pending = false;
+        modifier.error = '';
+        modifier.airlines = action.payload.airlines;
+        modifier.airlines_length = action.payload.airlines.length;
+        modifier.user_airlines = action.payload.user_airlines;
+        modifier.user_airlines_length = action.payload.user_airlines.length;
+      } else {
+        modifier.pending = false;
+        modifier.error = action.payload.error;
+        modifier.airlines = [];
+        modifier.airlines_length = 0;
+        modifier.user_airlines = [];
+        modifier.user_airlines_length = 0;
+      }
+    });
+
+    builder.addCase(createAirline.rejected, (state, action) => {
+      const modifier = state;
+      if (action.payload.error) {
+        modifier.pending = false;
+        modifier.error = action.payload.error;
+        modifier.airlines = [];
+        modifier.airlines_length = 0;
+        modifier.user_airlines = [];
+        modifier.user_airlines_length = 0;
       }
     });
   },
