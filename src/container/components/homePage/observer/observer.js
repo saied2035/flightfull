2f69a9c2ac0ab7919a0e3a50021e4896bf017b77
saied2035 <saied2421998@gmail.com>
2023-10-25 11:58:20 +0300

    determine minimum slides should be existed to render arrows

--- src/container/components/homePage/observer/observer.js
+++ src/container/components/homePage/observer/observer.js
@@ -1,11 +1,12 @@
 class Observer {
   constructor({
-    type, slide, setSlide, listLength, setDisabledNextArrow,
+    type, slide, setSlide, listLength, setDisabledNextArrow, setSlidesLength,
   }) {
     this.slide = slide;
     this.setSlide = setSlide;
     this.listLength = listLength;
     this.setDisabledNextArrow = setDisabledNextArrow;
+    this.setSlidesLength = setSlidesLength;
     if (type === 'resize') this.createResizeObserver();
     if (type === 'intersection') this.createIntersectionObserverForDisableNextArrow();
   }
@@ -13,15 +14,16 @@ class Observer {
   createResizeObserver() {
     this.resizeObserver = new ResizeObserver((entries) => {
       const entry = entries[0];
-      if (entry.contentRect.width >= 1536) {
+      this.checkSlidesLength(entry);
+      if (entry.contentRect.width >= 1536 && this.listLength > 4) {
         return this.slide > this.listLength - 4 ? this.setSlide(this.listLength - 4)
           : this.setSlide(this.slide);
       }
-      if (entry.contentRect.width >= 1024) {
+      if (entry.contentRect.width >= 1140 && this.listLength > 3) {
         return this.slide > this.listLength - 3 ? this.setSlide(this.listLength - 3)
           : this.setSlide(this.slide);
       }
-      if (entry.contentRect.width >= 900) {
+      if (entry.contentRect.width >= 900 && this.listLength > 2) {
         return this.slide > this.listLength - 2 ? this.setSlide(this.listLength - 2)
           : this.setSlide(this.slide);
       }
@@ -46,6 +48,23 @@ class Observer {
 
   observeIntersection(element) {
     this.intersectionObserver.observe(element);
+  }
+
+  protected
+
+  checkSlidesLength(entry) {
+    if (entry.contentRect.width < 900) {
+      this.setSlidesLength(1);
+    }
+    if (entry.contentRect.width >= 900) {
+      this.setSlidesLength(2);
+    }
+    if (entry.contentRect.width >= 1140) {
+      this.setSlidesLength(3);
+    }
+    if (entry.contentRect.width >= 1536) {
+      this.setSlidesLength(4);
+    }
   }
 }
 
